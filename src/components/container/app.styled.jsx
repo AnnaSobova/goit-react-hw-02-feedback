519ec47453edec21d958354d8f4f8048d011166b
Anna Sobova <anna_sobova@ukr.net>
2022-12-09 13:46:01 +0200

    styled

--- src/components/container/app.styled.jsx
+++ src/components/container/app.styled.jsx
@@ -1,11 +1,11 @@
 import styled from '@emotion/styled';
 
-export const Container = styled.div`
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
+const Container = styled.div`
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 36px;
+    color: #010101
 `
 export default Container
